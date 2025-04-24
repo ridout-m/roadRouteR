29d30d4957a06f5c4ed2599b3b46b4d0bab6b43b
ridout-m <mackenzie.ridout@defra.gov.uk>
2025-04-24 14:33:36 +0000

    remove "hello" and add package help file

---
+++ R/roadRouteR-package.R
@@ -0,0 +1,8 @@
+#' roadRouteR: Route Calculation and Mapping with OSRM + OpenStreetMap
+#'
+#' A minimal package for calculating and plotting realistic, drivable road routes using OpenStreetMap and OSRM.
+#'
+#' @docType package
+#' @name roadRouteR
+#' @keywords internal
+"_PACKAGE"
