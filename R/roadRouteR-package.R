a840af631b98dcde3e395991f9c7d5a60785d805
ridout-m <mackenzie.ridout@defra.gov.uk>
2025-04-24 14:54:46 +0000

    Fixed Package level docs

--- R/roadRouteR-package.R
+++ R/roadRouteR-package.R
@@ -1,8 +1,11 @@
 #' roadRouteR: Route Calculation and Mapping with OSRM + OpenStreetMap
 #'
-#' A minimal package for calculating and plotting realistic, drivable road routes using OpenStreetMap and OSRM.
+#' A lightweight wrapper for calculating and visualizing realistic road routes using OSRM and OpenStreetMap.
+#'
+#' @section Functions:
+#' - [getRoute()]: Calculate and optionally plot a road route between two places.
 #'
 #' @docType package
 #' @name roadRouteR
-#' @keywords internal
+#' @keywords package
 "_PACKAGE"
